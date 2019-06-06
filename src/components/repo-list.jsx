a8a4c862d1c7665a8f52454925fa90a740dd95e7
Mano M <manonmanim@visualbi.com>
2019-06-06 20:52:44 +0530

    RepoList updated

--- src/components/repo-list.jsx
+++ src/components/repo-list.jsx
@@ -1,13 +1,44 @@
 import React, { Component } from 'react';
 import Repo from "./repo";
 export default class RepoList extends Component {
+
+    state = {
+        repoList: []
+    };
+
+    componentDidMount() {
+        fetch('https://api.github.com/users/supreetsingh247/repos')
+            .then(res => res.json())
+            .then(result => {
+                const repoList = result.map(res => {
+                    if (res.name === 'first-contributions') {
+                        debugger
+                    }
+                    return {
+                        id: res.id,
+                        updated_at: res.updated_at,
+                        isPrivate: res.private,
+                        language: res.language,
+                        forks_count: res.forks_count,
+                        description: res.description,
+                        name: res.name
+                    }
+                });
+                this.setState({
+                    repoList
+                });
+            })
+            .catch(e => {
+                this.setState({
+                    repoList: []
+                });
+            })
+    }
     render() {
         return (<div className='repo-container'>
-            <Repo />
-            <Repo />
-            <Repo />
-            <Repo />
-            <Repo />
+            {
+                this.state.repoList.map(repo => <Repo id={repo.id} {...repo} />)
+            }
         </div>);
     }
 }
--- src/components/repo.jsx
+++ src/components/repo.jsx
@@ -3,12 +3,22 @@ import '../css/repo.css'
 
 export default class repo extends Component {
     render() {
+        const {
+            updated_at,
+            isPrivate,
+            language,
+            forks_count,
+            description,
+            name
+        } = this.props;
         return <div className='border-bottom flex-column-container'>
             <div>
                 <h3>
                     <a href="/VBIManonmaniM/BiFrost">
-                        BiFrost</a>
-                    <span className="label label-outline">Private</span>
+                        {name}</a>
+                    {
+                        isPrivate ? <span className="label label-outline">Private</span> : null
+                    }
                 </h3>
             </div>
             <div className="forked-from">
@@ -18,22 +28,22 @@ export default class repo extends Component {
             </div>
             <div className='description'>
                 <p className="default-font">
-                    DSX Suite
+                    {description}
                 </p>
             </div>
             <div className='flex-row-container'>
                 <span className="default-font programming-language">
                     <span className="repo-language-color"></span>
-                    <span className="programmingLanguage">JavaScript</span>
+                    <span className="programmingLanguage">{language}</span>
                 </span>
                 <span>
                     <a className="muted-link mr-3" href="/VBIManonmaniM/BiFrost/network/members">
                         <svg aria-label="fork" viewBox="0 0 10 16" version="1.1" width="10" height="16" role="img"><path d="M8 1a1.993 1.993 0 0 0-1 3.72V6L5 8 3 6V4.72A1.993 1.993 0 0 0 2 1a1.993 1.993 0 0 0-1 3.72V6.5l3 3v1.78A1.993 1.993 0 0 0 5 15a1.993 1.993 0 0 0 1-3.72V9.5l3-3V4.72A1.993 1.993 0 0 0 8 1zM2 4.2C1.34 4.2.8 3.65.8 3c0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zm3 10c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zm3-10c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2z"></path></svg>
-                        <span className='fork default-font'>13</span>
+                        <span className='fork default-font'>{forks_count}</span>
                     </a>
                 </span>
                 <span className='default-font'>
-                    Updated on Apr 27
+                    Updated on {updated_at}
                 </span>
             </div>
         </div>
