49b2e5c5618b63e44c97161a34402130b55b0ca4
Mano M <manonmanim@visualbi.com>
2019-06-06 21:08:51 +0530

    mobx and mobx-react added

--- src/components/repo-list.jsx
+++ src/components/repo-list.jsx
@@ -2,39 +2,11 @@ import React, { Component } from 'react';
 import Repo from "./repo";
 export default class RepoList extends Component {
 
-    state = {
-        repoList: []
-    };
-
-    componentDidMount() {
-        fetch('https://api.github.com/users/vbimanonmanim/repos')
-            .then(res => res.json())
-            .then(result => {
-                const repoList = result.map(res => {
-                    return {
-                        id: res.id,
-                        updated_at: res.updated_at,
-                        isPrivate: res.private,
-                        language: res.language,
-                        forks_count: res.forks_count,
-                        description: res.description,
-                        name: res.name
-                    }
-                });
-                this.setState({
-                    repoList
-                });
-            })
-            .catch(e => {
-                this.setState({
-                    repoList: []
-                });
-            })
-    }
     render() {
+        const { repoList = [] } = this.props;
         return (<div className='repo-container'>
             {
-                this.state.repoList.map(repo => <Repo id={repo.id} {...repo} />)
+                repoList.map(repo => <Repo id={repo.id} {...repo} />)
             }
         </div>);
     }
--- src/container/index.jsx
+++ src/container/index.jsx
@@ -5,6 +5,33 @@ import FilterPanel from "../components/filter-panel";
 import '../css/app.css';
 
 class Container extends Component {
+    state = {};
+
+    componentDidMount() {
+        fetch('https://api.github.com/users/vbimanonmanim/repos')
+            .then(res => res.json())
+            .then(result => {
+                const repoList = result.map(res => {
+                    return {
+                        id: res.id,
+                        updated_at: res.updated_at,
+                        isPrivate: res.private,
+                        language: res.language,
+                        forks_count: res.forks_count,
+                        description: res.description,
+                        name: res.name
+                    }
+                });
+                this.setState({
+                    repoList
+                });
+            })
+            .catch(e => {
+                this.setState({
+                    repoList: []
+                });
+            })
+    }
     render() {
         return <div className='flex-row-container'>
             <div className='padding-4'> 
@@ -12,10 +39,10 @@ class Container extends Component {
             </div>
             <div className='padding-4 flex-column-container'>
                 <div>
-                    <FilterPanel />
+                    <FilterPanel/>
                 </div>
                 <div>
-                    <RepoList />
+                    <RepoList repoList={this.state.repoList}/>
                 </div>
             </div>
         </div>;
